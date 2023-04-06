69faab31b93ff203ab539eb5f42977fa38457260
lightnonstop <umichaeledet003@gmail.com>
2023-04-06 12:47:17 +0000

    Change from synchronous request to using promise.all

---
+++ src/components/StockList.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import finnHub from "../../apis/FinnHub.js";
+const StockList = () => {
+  const [stock, setStock] = useState();
+  const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
+
+  useEffect(() => {
+    let isMounted = true;
+    const fetchData = async () => {
+      try {
+        const responses = Promise.all(finnHub.get("/quote", {
+          params: {
+            symbol: "GOOGL",
+          },
+        }), finnHub.get("/quote", {
+          params: {
+            symbol: "MSFT",
+          },
+        }), finnHub.get("/quote", {
+          params: {
+            symbol: "AMZN",
+          },
+        })
+        )
+        console.log(responses);
+        if (isMounted) {
+          setStock(responses);
+        }
+      } catch (error) {}
+    };
+    fetchData();
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+  return <div>StockList</div>;
+};
+
+export default StockList;
