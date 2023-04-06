ea48848b30ffe02afaa1729ccb0427713bd4040f
lightnonstop <umichaeledet003@gmail.com>
2023-04-06 23:17:31 +0000

    null coalescing used for local storage

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { BsFillCaretDownFill, BsFillCaretUpFill } from 'react-icons/bs';
 import finnHub from "../../apis/FinnHub.js";
+import { WatchListContext } from "../context/WatchListContext.jsx";
 const StockList = () => {
-  const [stock, setStock] = useState();
-  const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
+  const [stock, setStock] = useState([]);
+  const {watchList, deleteStock} = useContext(WatchListContext);
+  const navigate = useNavigate();
+  
+  const changeColor = change => {
+    return change > 0 ? 'success' : 'danger';
+  }
 
+  const renderIcon = change => {
+    return change > 0 ? <BsFillCaretUpFill/> : <BsFillCaretDownFill/>
+  }
   useEffect(() => {
     let isMounted = true;
     const fetchData = async () => {
-      const responses = [];
       try {
         const responses = await Promise.all(watchList.map(stock => {
           return finnHub.get('/quote', {
@@ -19,9 +29,14 @@ const StockList = () => {
 
           )
         }))
-        console.log(responses);
+        const data = responses.map(response => {
+          return { 
+            data: response.data,
+            symbol: response.config.params.symbol
+          }
+        })
         if (isMounted) {
-          setStock(responses);
+          setStock(data);
         }
       } catch (error) {}
     };
@@ -29,8 +44,50 @@ const StockList = () => {
     return () => {
       isMounted = false;
     };
-  }, []);
-  return <div>StockList</div>;
+  }, [watchList]);
+
+
+  const handleStockSelect = (symbol) => {
+    navigate(`detail/${symbol}`)
+  }
+  return (
+    <table className="table hover mt-5">
+      <thead style={{color: 'rgb(79, 89, 102'}}>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Last</th>
+          <th scope="col">Chg</th>
+          <th scope="col">Chg%</th>
+          <th scope="col">High</th>
+          <th scope="col">Low</th>
+          <th scope="col">Open</th>
+          <th scope="col">Pclose</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+        stock.map((stockData) => {
+          return (
+            <tr style={{cursor: 'pointer'}} onClick={() => handleStockSelect(stockData.symbol)} className="table-row" key={stockData.symbol}>
+              <th scope="row">{stockData.symbol}</th>
+              <td>{stockData.data.c}</td>
+              <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.d}{renderIcon(stockData.data.d)}</td>
+              <td className={`text-${changeColor(stockData.data.dp)}`}>{stockData.data.dp}{renderIcon(stockData.data.dp)}</td>
+              <td>{stockData.data.h}</td>
+              <td>{stockData.data.l}</td>
+              <td>{stockData.data.o}</td>
+              <td>{stockData.data.pc} <button className="btn btn-danger btn-sm ml-3 d-inline-block delete-button" onClick={(e) => {
+                e.stopPropagation();
+                deleteStock(stockData.symbol)
+                console.log(deleteStock);
+              }}>Remove</button></td>
+            </tr>
+          )
+        })
+        }
+      </tbody>
+    </table>
+  );
 };
 
 export default StockList;
